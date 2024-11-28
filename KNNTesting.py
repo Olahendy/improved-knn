64c13a0c365190139a34c67850548aec8301b656
Todd Heino <todd.a.heino@gmail.com>
2024-11-28 11:34:55 -0800

    Starting to introduce all the datasets

--- KNNTesting.py
+++ KNNTesting.py
@@ -7,23 +7,26 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.preprocessing import minmax_scale
 
-dataset1=pd.read_csv("DataSets/D1heart.csv", sep=',')
-dataset1 = dataset1.dropna(axis=0, how='any', subset=None, inplace=False)
+dataFiles = ["D1heart.csv", "D2heartoutcomes.csv", "D3diabetes.csv", "D4Heart_Disease_Prediction.csv", "D7diabetes.csv", "D8Breast_cancer_data.csv"]
 
-X = dataset1.iloc[:, :-1].values
-y = dataset1.iloc[:, -1].values
+for dataFile in dataFiles:
+    dataset=pd.read_csv("DataSets/" + dataFile, sep=',')
+    dataset = dataset.dropna(axis=0, how='any', subset=None, inplace=False)
 
-X = minmax_scale(X, axis=0)
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, -1].values
 
-for i in range(1, 10, 2): # 1, 3, 5, 7, 9
-    print("K = ", i)
-    model=KNeighborsClassifier(n_neighbors=i)
-    crossVSAcc=cross_val_score(model, X, y, cv=10, scoring='accuracy')
-    crossVSPrecision=cross_val_score(model, X, y, cv=10, scoring='precision')
-    crossVSRecall=cross_val_score(model, X, y, cv=10, scoring='recall')
-    accuracy = crossVSAcc.mean()
-    precision = crossVSPrecision.mean()
-    recall = crossVSRecall.mean()
-    print("accuracy: ", accuracy)
-    print("precision: ", precision)
-    print("recall: ", recall)
+    X = minmax_scale(X, axis=0)
+
+    for i in range(1, 10, 2): # 1, 3, 5, 7, 9
+        print("K = ", i)
+        model=KNeighborsClassifier(n_neighbors=i)
+        crossVSAcc=cross_val_score(model, X, y, cv=10, scoring='accuracy')
+        crossVSPrecision=cross_val_score(model, X, y, cv=10, scoring='precision')
+        crossVSRecall=cross_val_score(model, X, y, cv=10, scoring='recall')
+        accuracy = crossVSAcc.mean()
+        precision = crossVSPrecision.mean()
+        recall = crossVSRecall.mean()
+        print("accuracy: ", accuracy)
+        print("precision: ", precision)
+        print("recall: ", recall)
