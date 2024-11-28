227f5890f8f58c620e844e8e50cfd22355703081
Todd Heino <todd.a.heino@gmail.com>
2024-11-28 10:46:27 -0800

    Starting basic KNN, adding lock files to gitignore

--- KNNTesting.py
+++ KNNTesting.py
@@ -1,5 +1,14 @@
 import pandas as pd
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import minmax_scale
 
 dataset1=pd.read_csv("DataSets/D1heart.csv", sep=',')
 dataset1 = dataset1.dropna(axis=0, how='any', subset=None, inplace=False)
+
+X = dataset1.iloc[:, :-1].values
+y = dataset1.iloc[:, -1].values
+
